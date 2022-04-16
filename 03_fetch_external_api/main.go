7725e3e66ad256dbfea560ef2456a9308c9a96b7
pierangelo1982 <pierangelo1982@gmail.com>
2022-04-16 10:33:53 +0200

    improved 03_fetch_external_api

--- 03_fetch_external_api/main.go
+++ 03_fetch_external_api/main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"sync"
 	"time"
 )
 
@@ -20,7 +21,6 @@ func main() {
 	defer rosterFile.Close()
 
 	wrt := io.MultiWriter(os.Stdout, rosterFile)
-
 	log.SetOutput(wrt)
 
 	teams, err := nhlApi.GetAllTeams()
@@ -28,13 +28,46 @@ func main() {
 		log.Fatal("errore nell'ottenere i teams: %v", err)
 	}
 
-	for _, team := range teams {
-		log.Println("---------------------------")
-		log.Printf("Name %s", team.Name)
+	var wg sync.WaitGroup
+	wg.Add(len(teams))
 
-		log.Println("---------------------------")
+	results := make(chan []nhlApi.Roster)
+
+	for _, team := range teams {
+		go func(team nhlApi.Team) {
+			roster, err := nhlApi.GetRosters(team.ID)
+			if err != nil {
+				log.Fatal("errore nell'ottenere i roster: %v", err)
+			}
+
+			results <- roster
+
+			wg.Done()
+		}(team)
 	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	log.Println(results)
+	display(results)
 
 	// tempo impiegato
 	log.Printf("took %v", time.Now().Sub(now))
+}
+
+func display(results chan []nhlApi.Roster) {
+	for r := range results {
+		for _, ros := range r {
+			log.Println("---------------------------")
+			log.Printf("ID %d\n", ros.Person.ID)
+			log.Printf("Name %s\n", ros.Person.FullName)
+			log.Printf("Position %s\n", ros.Position.Abbreviation)
+			log.Printf("Jersey %s\n", ros.JerseyNumber)
+			log.Println("---------------------------")
+		}
+
+	}
 }
